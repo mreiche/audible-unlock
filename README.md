c85070483f5475ffd0586f86a2be1c9771997380
Mike Reiche <mike@reiche.world>
2022-05-24 11:01:06 +0200

    Docker bugfixes

--- README.md
+++ README.md
@@ -17,10 +17,20 @@ This shell script unlocks all `.aax` files using rainbow tables from _rcrack_.
   choco install ffmpeg cygwin
   ```
 - Open _cygwin_ shell in the audible download folder
-- Run `./audible-unlock.sh`
+- Run `bash audible-unlock.sh`
 
 ### On Linux
 
 - Install _ffmpeg_ with a package manager of your choice
 - Go to your audible download folder
-- Run `./audible-unlock.sh`
+- Run `bash audible-unlock.sh`
+
+### Docker
+
+```shell
+docker run -it --volume=/Users/mike.reiche//Downloads:/data ubuntu /bin/bash
+```
+```shell
+apt-get update
+apt-get install ffmpeg libavcodec-extra gawk wine
+```
--- audible-unlock.sh
+++ audible-unlock.sh
@@ -9,19 +9,26 @@
 # See: https://github.com/inAudible-NG/tables
 
 INTRO_OUTRO_SEC=3
+PWD=$(pwd)
+RCRACK_BIN="$PWD/tables/run/rcrack.exe"
 
 if [[ "$OSTYPE" == "cygwin" ]]; then
-  RCRACK_CMD="tables/run/rcrack.exe"
+  RCRACK_CMD="$RCRACK_BIN"
 else
-  RCRACK_CMD="tables/rcrack"
+  WINE=$(which wine)
+  RCRACK_CMD="$WINE $RCRACK_BIN"
 fi
 
+FFPROBE=$(which ffprobe)
+FFMPEG=$(which ffmpeg)
+GAWK=$(which gawk)
+
 for filepath in *.aax; do
-  ffprobe.exe "$filepath" -- 2>&1 | cat - > out.txt
-  checksum=$(gawk 'match($0, /file checksum == ([A-z0-9]+)/, a) { print a[1] }' out.txt)
-  hours=$(gawk 'match($0, /Duration: ([0-9]+):([0-9]+):([0-9]+)/, a) { print a[1] }' out.txt)
-  minutes=$(gawk 'match($0, /Duration: ([0-9]+):([0-9]+):([0-9]+)/, a) { print a[2] }' out.txt)
-  seconds=$(gawk 'match($0, /Duration: ([0-9]+):([0-9]+):([0-9]+)/, a) { print a[3] }' out.txt)
+  $FFPROBE "$filepath" -- 2>&1 | cat - > out.txt
+  checksum=$("$GAWK" 'match($0, /file checksum == ([A-z0-9]+)/, a) { print a[1] }' out.txt)
+  hours=$("$GAWK" 'match($0, /Duration: ([0-9]+):([0-9]+):([0-9]+)/, a) { print a[1] }' out.txt)
+  minutes=$("$GAWK" 'match($0, /Duration: ([0-9]+):([0-9]+):([0-9]+)/, a) { print a[2] }' out.txt)
+  seconds=$("$GAWK" 'match($0, /Duration: ([0-9]+):([0-9]+):([0-9]+)/, a) { print a[3] }' out.txt)
   echo checksum \'$checksum\'
   echo duration $hours:$minutes:$seconds
   let "seconds = $seconds - 2*$INTRO_OUTRO_SEC"
@@ -29,13 +36,29 @@ for filepath in *.aax; do
     let minutes--
     let "seconds = 60 + $seconds"
   fi
+
+  if [[ -z $checksum ]]; then
+    echo "Unable to read checksum"
+    exit 1
+  fi
+
   echo target duration $hours:$minutes:$seconds
-  echo "search for key..."
+  echo "Searching for key..."
   $RCRACK_CMD tables -h "$checksum" > key.txt
-  hexkey=$(gawk 'match($0, /hex:([A-z0-9]+)/, a) { print a[1] }' key.txt)
+  if [[ $? != 0 ]]; then
+    echo "Failed reading key  ($RCRACK_CMD tables -h $checksum > key.txt)"
+    exit 1
+  fi
+
+  hexkey=$("$GAWK" 'match($0, /hex:([A-z0-9]+)/, a) { print a[1] }' key.txt)
   filename=$(basename -- "$filepath")
   extension="${filename##*.}"
   filename="${filename%.*}"
-  echo "decode file..."
-  ffmpeg.exe -activation_bytes $hexkey -i "$filepath" -c:a copy -vn -ss 3.6 -t $hours:$minutes:$seconds "$filename.mp4"
+  echo "Decoding file..."
+  $FFMPEG -activation_bytes "$hexkey" -i "$filepath" -c:a copy -vn -ss 3.6 -t $hours:$minutes:$seconds "$filename.mp4"
+  if [[ $? != 0 ]]; then
+    echo "Failed decoding ($FFMPEG -activation_bytes $hexkey -i $filepath -c:a copy -vn -ss 3.6 -t $hours:$minutes:$seconds $filename.mp4)"
+    exit 1
+  fi
+
 done
